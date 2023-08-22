3e607fa14ae44537e74a0c2bb2576600254bb135
Issam <Issam.aboulfadl05@gmail.com>
2023-08-23 00:33:26 +0100

    fix process env applay

--- pages/checkout.js
+++ pages/checkout.js
@@ -8,7 +8,7 @@ import { useSession } from "next-auth/client";
 import { loadStripe } from "@stripe/stripe-js";
 import axios from "axios";
 
-const stripePromise = loadStripe(process.env.STRIPE_PUBLIC_KEY);
+const stripePromise = loadStripe(process.env.stripe_public_key);
 
 const checkout = () => {
 	const items = useSelector(selectItem);
