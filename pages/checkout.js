d721bed72ba3d06a89130c5085653f5052a53f47
Issam <Issam.aboulfadl05@gmail.com>
2023-08-22 17:44:21 +0100

    fix some change

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,97 +1,95 @@
-import React from 'react';
-import Image from 'next/image';
-import { useSelector } from 'react-redux';
-import { selectItem, selectTotal } from '../slice/basketSlice';
-import CheckoutProduct from '../components/CheckoutProduct';
-import Currency from 'react-currency-formatter';
-import { useSession } from 'next-auth/client';
-import { loadStripe } from '@stripe/stripe-js';
-import axios from 'axios';
+import React from "react";
+import Image from "next/image";
+import { useSelector } from "react-redux";
+import { selectItem, selectTotal } from "../slice/basketSlice";
+import CheckoutProduct from "../components/CheckoutProduct";
+import Currency from "react-currency-formatter";
+import { useSession } from "next-auth/client";
+import { loadStripe } from "@stripe/stripe-js";
+import axios from "axios";
 
-const stripePromise = loadStripe(process.env.stripe_public_key);
+const stripePromise = loadStripe(process.env.STRIPE_PUBLIC_KEY);
 
 const checkout = () => {
-  const items = useSelector(selectItem);
-  const total = useSelector(selectTotal);
-  const [session] = useSession();
-  // console.log('item is', items);
-  const createCheckoutSession = async () => {
-    const stripe = await stripePromise;
+	const items = useSelector(selectItem);
+	const total = useSelector(selectTotal);
+	const [session] = useSession();
+	// console.log('item is', items);
+	const createCheckoutSession = async () => {
+		const stripe = await stripePromise;
 
-    // cal the backend to create a checkout session...
-    const checkoutSession = await axios.post('/api/create-checkout-session', {
-      items: items,
-      email: session.user.email,
-    });
+		// cal the backend to create a checkout session...
+		const checkoutSession = await axios.post("/api/create-checkout-session", {
+			items: items,
+			email: session.user.email,
+		});
 
-    // Redirect user/customer to Stripe Checkout
-    const result = await stripe.redirectToCheckout({
-      sessionId: checkoutSession.data.id,
-    });
-    if (result.error) {
-      alert(result.error.message);
-    }
-  };
-  return (
-    <div className="bg-gray-100">
-      <main className="lg:flex max-w-screen-2xl mx-auto">
-        {/* Left */}
-        <div className="flex-grow m-5 shadow-sm">
-          <Image
-            src="/images/ecommerce-.png"
-            width={1020}
-            height={250}
-            objectFit="contain"
-          />
-          <div className="flex flex-col p-5 space-y-10 bg-white">
-            <h1 className="text-3xl border-b pb-4 ">
-              {items.length === 0
-                ? 'Your marketplace basket is empty'
-                : 'Shopping Basket'}
-            </h1>
+		// Redirect user/customer to Stripe Checkout
+		const result = await stripe.redirectToCheckout({
+			sessionId: checkoutSession.data.id,
+		});
+		if (result.error) {
+			alert(result.error.message);
+		}
+	};
+	return (
+		<div className="bg-gray-100">
+			<main className="lg:flex max-w-screen-2xl mx-auto">
+				{/* Left */}
+				<div className="flex-grow m-5 shadow-sm">
+					<Image
+						src="/images/ecommerce-.png"
+						width={1020}
+						height={250}
+						objectFit="contain"
+					/>
+					<div className="flex flex-col p-5 space-y-10 bg-white">
+						<h1 className="text-3xl border-b pb-4 ">
+							{items.length === 0
+								? "Your marketplace basket is empty"
+								: "Shopping Basket"}
+						</h1>
 
-            {items.map((item, i) => (
-              <CheckoutProduct
-                key={i}
-                id={item.id}
-                title={item.title}
-                rating={item.rating}
-                description={item.description}
-                price={item.price}
-                category={item.category}
-                image={item.image}
-                hasPrime={item.hasPrism}
-              />
-            ))}
-          </div>
-        </div>
-        {/* Right */}
-        <div className="flex flex-col bg-white p-10 shadow-md">
-          {items.length > 0 && (
-            <>
-              <h className="whitespace-nowrap">
-                SubTotal ({items.length} items)
-              </h>
-              <span className="font-bold">
-                <Currency quantity={total} currency="mad" />
-              </span>
-              <button
-                onClick={createCheckoutSession}
-                role="link"
-                disabled={!session}
-                className={`button mt-2 ${
-                  !session &&
-                  'from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed'
-                }`}
-              >
-                {!session ? 'Sign in to checkout' : 'Proceed to checkout'}
-              </button>
-            </>
-          )}
-        </div>
-      </main>
-    </div>
-  );
+						{items.map((item, i) => (
+							<CheckoutProduct
+								key={i}
+								id={item.id}
+								title={item.title}
+								rating={item.rating}
+								description={item.description}
+								price={item.price}
+								category={item.category}
+								image={item.image}
+								hasPrime={item.hasPrism}
+							/>
+						))}
+					</div>
+				</div>
+				{/* Right */}
+				<div className="flex flex-col bg-white p-10 shadow-md">
+					{items.length > 0 && (
+						<>
+							<h className="whitespace-nowrap">SubTotal ({items.length} items)</h>
+							<span className="font-bold">
+								<Currency quantity={total} currency="mad" />
+							</span>
+							<button
+								onClick={createCheckoutSession}
+								role="link"
+								disabled={!session}
+								className={`button mt-2 ${
+									!session &&
+									"from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed"
+								}`}
+							>
+								{!session ? "Sign in to checkout" : "Proceed to checkout"}
+							</button>
+						</>
+					)}
+				</div>
+			</main>
+		</div>
+	);
 };
 
 export default checkout;
